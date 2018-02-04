d00ef6021a9794728de0e17f35f3a5277367e375
Sunil <sunhick@gmail.com>
2018-02-03 17:54:00 -0800

    Intending settings.org and no theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -36,24 +36,3 @@
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
-
-(use-package dracula-theme
-          :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (dracula)))
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (gradle-mode neotree company-c-headers org-bullets org-bullets-mode clang-format magit iedit autopair which-key counsel-projectile counsel use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
