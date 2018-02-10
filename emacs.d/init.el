c0c355c0312b70823a6994780d4a2bbc56332e27
Sunil <Sunhick@gmail.com>
2018-02-10 11:52:21 -0800

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,11 +32,15 @@
  (expand-file-name "settings.org"
                    user-emacs-directory))
 
+(use-package reverse-theme
+  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (reverse)))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
