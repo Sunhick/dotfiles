53c5d55982a949540ddfe3155ebabbdacc00c0e0
Sunil <sunhick@gmail.com>
2018-06-23 19:30:52 -0700

    use grey theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,7 +32,7 @@
  (expand-file-name "settings.org"
                    user-emacs-directory))
 
-(use-package reverse-theme
+(use-package arjen-grey
   :ensure t)
 
 (custom-set-variables
@@ -40,11 +40,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (reverse)))
+ '(custom-enabled-themes (quote (arjen-grey)))
+ '(custom-safe-themes
+   (quote
+    ("83db918b06f0b1df1153f21c0d47250556c7ffb5b5e6906d21749f41737babb7" default)))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (yaml-mode haskell-mode expand-region plantuml-mode markdown-mode diminish multiple-cursors gradle-mode groovy-mode multi-term cmake-mode google-c-style smooth-scrolling auto-complete-clang auto-complete-c-headers auto-complete ggtags yasnippet-snippets yasnippet org-bullets clang-format switch-window iedit autopair which-key counsel-projectile counsel magit use-package))))
+    (arjen-grey-theme yaml-mode haskell-mode expand-region plantuml-mode markdown-mode diminish multiple-cursors gradle-mode groovy-mode multi-term cmake-mode google-c-style smooth-scrolling auto-complete-clang auto-complete-c-headers auto-complete ggtags yasnippet-snippets yasnippet org-bullets clang-format switch-window iedit autopair which-key counsel-projectile counsel magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
