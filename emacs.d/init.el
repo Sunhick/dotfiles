66bbde6b1a09ca0d01f4a3018cd07243c798357f
Sunil <sunhick@gmail.com>
2018-06-24 20:16:46 -0700

    using solarized dark theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -32,7 +32,10 @@
  (expand-file-name "settings.org"
                    user-emacs-directory))
 
-(use-package arjen-grey-theme
+;; (use-package arjen-grey-theme
+;;   :ensure t)
+
+(use-package solarized-theme
   :ensure t)
 
 (custom-set-variables
@@ -40,10 +43,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (arjen-grey)))
- '(custom-safe-themes
-   (quote
-    ("83db918b06f0b1df1153f21c0d47250556c7ffb5b5e6906d21749f41737babb7" default)))
+ '(custom-enabled-themes (quote (solarized-dark))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
