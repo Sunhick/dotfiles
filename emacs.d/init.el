48d2890d0f18e255f922ab34d09270741cfce548
Sunil <Sunhick@gmail.com>
2018-06-27 23:50:51 -0700

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -35,7 +35,10 @@
 ;; (use-package arjen-grey-theme
 ;;   :ensure t)
 
-(use-package solarized-theme
+;; (use-package solarized-theme
+;;   :ensure t)
+
+(use-package dracula-theme
   :ensure t)
 
 (custom-set-variables
@@ -43,7 +46,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
