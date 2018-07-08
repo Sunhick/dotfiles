595483baa0b38355f95e895525fbccf56831978a
Sunil <Sunhick@gmail.com>
2018-07-08 10:17:16 -0700

    Update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,14 +44,16 @@
 ;; (use-package solarized-theme
 ;;   :ensure t)
 
-(use-package nova-theme
+;; (use-package nova-theme
+;;   :ensure t)
+(use-package reverse-theme
   :ensure t)
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
               (lambda (frame)
                 (with-selected-frame frame
-                  (load-theme 'nova t))))
+                  (load-theme 'reverse t))))
   (load-theme 'nova t))
 
 (custom-set-variables
