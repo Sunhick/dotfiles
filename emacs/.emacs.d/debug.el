14d75e48f827c838a45b22f420925ff25848f550
Sunil <sunhick@gmail.com>
2018-07-17 01:32:41 -0700

    updated debug to midnight theme

--- emacs/.emacs.d/debug.el
+++ emacs/.emacs.d/debug.el
@@ -1,16 +1,14 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; debug.el - is only for debugging purposes and shouldn't be
 ;;        taken as a replacement for init.el or settings.org.
-;; 
-;; $ emacs -q --load emacs.d/debug.el 
-;; 
+;;
+;; $ emacs -q --load emacs.d/debug.el
+;;
 ;; Author : Sunil
 ;; Email : sunhick@gmail.com
 ;;
 ;; copyright (c) 2018. All rights reserved.
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -35,10 +33,17 @@
     (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+(set-default 'indicate-empty-lines t)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'midnight t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ )
