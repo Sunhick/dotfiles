d177db3bb19d8aa11686f8b982bf1b2c967981d6
Sunil <sunhick@gmail.com>
2018-12-05 22:15:50 -0800

    reconfig emacs with optimizations

--- emacs/.emacs.d/debug.el
+++ emacs/.emacs.d/debug.el
@@ -21,30 +21,13 @@
 ;; start package.el with emacs
 (require 'package)
 
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+;; add MELPA to repository list
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-;; Emacs title bar customizations
-(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-(add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon nil)
-(setq frame-title-format nil)
-(setq frame-resize-pixelwise t)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-(set-default 'indicate-empty-lines t)
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'midnight t)
+(org-babel-load-file
+ (expand-file-name "emacs.config.org"
+                   user-emacs-directory))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
