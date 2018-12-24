a47cc20dd3d264f1f2dad88776f0e184de9931b6
Sunil <sunhick@gmail.com>
2018-12-23 20:18:29 -0800

    remove org mode emacs configurations

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,35 +1,72 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init.el --- emacs configurations
 ;;
-;; Author : Sunil
-;; Email : sunhick@gmail.com
+;; Copyright (c) 2018-2019 Sunil
 ;;
-;; copyright (c) 2018. All rights reserved.
+;; Author: Sunil <sunhick@gmail.com>
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; Emacs configuration file
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-(package-initialize)
+;;; Code:
 
-(setq user-full-name "Sunil"
-      user-mail-address "sunhick@gmail.com")
+(defvar emacs-dir (file-name-directory load-file-name)
+  "Directory hosting the emacs configuration files")
 
-;; start package.el with emacs
-(require 'package)
+(defvar emacs-base-dir (expand-file-name "base" emacs-dir)
+  "Emacs base directory housing the heart of emacs configurations")
 
-;; add MELPA to repository list
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+(defvar emacs-user-dir (expand-file-name "user" emacs-dir)
+  "Emacs user's directory")
 
-(org-babel-load-file
- (expand-file-name "emacs.config.org"
-                   user-emacs-directory))
+(defvar emacs-themes-dir (expand-file-name "themes" emacs-dir)
+  "Directory hosting the themes for emacs")
 
+(defvar emacs-packages-dir (expand-file-name "packages" emacs-dir)
+  "Emacs packages directory boostrapping the installed packages.
+Most of them will be keybindings for packages installed by default.")
+
+(add-to-list 'load-path emacs-base-dir)
+(add-to-list 'load-path emacs-user-dir)
+(add-to-list 'load-path emacs-themes-dir)
+(add-to-list 'load-path emacs-packages-dir)
+
+(require 'base-init)
+(require 'user-setup)
+(require 'setup-theme)
+(require 'pkg-initializer)
+
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (diminish auto-complete switch-window guide-key smartparens multiple-cursors magit ido-completing-read+ ido-vertical-mode flx-ido fzf zenburn-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
