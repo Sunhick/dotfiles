fd3f8b17d4058aac4f8d2ae7905e683a63348db4
Sunil <sunhick@gmail.com>
2022-02-19 13:31:27 -0800

    disable relative line numbers

--- emacs/.emacs.d/base/base-ui.el
+++ emacs/.emacs.d/base/base-ui.el
@@ -92,11 +92,11 @@
 ;; Works only with emacs >= 26
 ;; use native relative line numbering(faster than linum-relative package)
 ;; Builtin package also fixes the issues with code folding.
-(when (not (version< emacs-version "26.0"))
-  (progn
-    (global-display-line-numbers-mode)
-    (setq-default display-line-numbers-type 'visual
-                  display-line-numbers-current-absolute nil)))
+;; (when (not (version< emacs-version "26.0"))
+;;   (progn
+;;     (global-display-line-numbers-mode)
+;;     (setq-default display-line-numbers-type 'visual
+;;                   display-line-numbers-current-absolute nil)))
 
 (provide 'base-ui)
 
